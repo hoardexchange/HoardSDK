01ee1002452c4d5e921ebf5d6ba8a0e373a8a580
rodia <radek.zagorowicz@gmail.com>
2018-02-24 14:47:51 +0100

    new main contract address.

--- HoardSDK/BC/BCComm.cs
+++ HoardSDK/BC/BCComm.cs
@@ -16,7 +16,7 @@ namespace Hoard.BC
         private Web3 web = null;
         private GameCenterContract gameCenter = null;
         
-        private const string GameInfoAddress = "0x9a342842b7a3409a76c5355a0085749a3c59da17";
+        private const string GameInfoAddress = "0x8d19fa3a98c56ad6de41ec15969abfe19923cffc";
 
         public BCComm(Nethereum.JsonRpc.Client.IClient client, Account account)
         {
