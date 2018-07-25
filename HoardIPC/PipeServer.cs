9d3f19984f26a28efea2033759cef1468a40b478
rafalw <rw@hoard.exchange>
2018-07-25 09:18:11 +0200

    [SDK-49] HoardApp<->Game communication layer (implementation)

--- HoardIPC/PipeServer.cs
+++ HoardIPC/PipeServer.cs
@@ -15,63 +15,56 @@ namespace HoardIPC
         protected Thread ServerThread;
         protected bool IsRunning;
         protected bool IsCLosed;
-        protected NamedPipeServerStream namedPipeServer;
-
-        private void WaitForConnectionCallBack(IAsyncResult iar)
-        {
-            try
-            {
-                // Get the pipe
-                NamedPipeServerStream pipeServer = (NamedPipeServerStream)iar.AsyncState;
-
-                // End waiting for the connection
-                pipeServer.EndWaitForConnection(iar);
-
-                // Recursively wait for the connection again and again....
-                pipeServer.BeginWaitForConnection(new AsyncCallback(WaitForConnectionCallBack), pipeServer);
-            }
-            catch
-            {
-            }
-        }
-
-        private static async Task<bool> ConnectAsync(NamedPipeServerStream pipe)
-        {
-            CancellationTokenSource cts = new CancellationTokenSource(1000);
-            await pipe.WaitForConnectionAsync(cts.Token);
-            return pipe.IsConnected;
-        }
+        protected NamedPipeServerStream NamedPipeServer;
 
         private static void ServerThreadLoop(object data)
         {
             PipeServer pipeServer = (PipeServer)data;
-            using (pipeServer.namedPipeServer = new NamedPipeServerStream("HoardPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous))
+            using (pipeServer.NamedPipeServer = new NamedPipeServerStream("HoardPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous))
             {
-                byte[] messageBuffer = new byte[PipeMessage.MessageChunkSize];
-                StringBuilder messageBuilder = new StringBuilder();
-                //pipeServer.namedPipeServer.BeginWaitForConnection(new AsyncCallback(pipeServer.WaitForConnectionCallBack), pipeServer.namedPipeServer);
+                byte[] messageBuffer = new byte[PipeHelper.MessageChunkSize];
+                byte[] headerBuffer = new byte[PipeHelper.HeaderChunkSize];
                 while (pipeServer.IsRunning)
                 {
                     System.Threading.Thread.Sleep(0);
                     try
                     {
-                        pipeServer.namedPipeServer.WaitForConnection();
-                        string messageChunk = string.Empty;
-                        messageBuilder.Clear();
+                        if (pipeServer.NamedPipeServer.IsConnected == false)
+                            pipeServer.NamedPipeServer.WaitForConnection();
                         messageBuffer = Enumerable.Repeat((byte)0, messageBuffer.Length).ToArray();
-                        do
+                        headerBuffer = Enumerable.Repeat((byte)0, headerBuffer.Length).ToArray();
+
+                        // read header
+                        int readBytes = pipeServer.NamedPipeServer.Read(headerBuffer, 0, headerBuffer.Length);
+                        if (readBytes > 0)
                         {
-                            int readBytes = pipeServer.namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
-                            messageChunk = Encoding.UTF8.GetString(messageBuffer);
-                            messageBuilder.Append(messageChunk);
+                            PipeHeader header = new PipeHeader();
+                            header.Desserialize(headerBuffer);
+                            Debug.Assert(header.msgSize <= PipeHelper.MessageChunkSize);
+                            readBytes = pipeServer.NamedPipeServer.Read(messageBuffer, 0, header.msgSize);
+
+                            switch(header.msgId)
+                            {
+                                case Messages.MSG_REGISTER_GAME:
+                                    {
+                                        RegisterGame msg = new RegisterGame();
+                                        msg.Desserialize(messageBuffer);
+                                    }
+                                    break;
+                                case Messages.MSG_UNREGISTER_GAME:
+                                    {
+                                        UnregisterGame msg = new UnregisterGame();
+                                        msg.Desserialize(messageBuffer);
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
-                        while (!pipeServer.namedPipeServer.IsMessageComplete);
-                        PipeMessage msg = JsonConvert.DeserializeObject<PipeMessage>(messageBuilder.ToString());
-                        //Console.WriteLine("Customer {0} has ordered {1} {2} with delivery address {3}", order.CustomerName, order.Quantity, order.ProductName, order.Address);
+                        //
                     }
                     catch (IOException e)
                     {
-
                     }
                 }
                 pipeServer.IsCLosed = true;
@@ -96,14 +89,14 @@ namespace HoardIPC
         public void Shutdown()
         {
             IsRunning = false;
-            namedPipeServer.Close();
+            NamedPipeServer.Close();
             while (!IsCLosed)
             {
                 System.Threading.Thread.Sleep(1);
             }
-            if (namedPipeServer.IsConnected)
-                namedPipeServer.Disconnect();
-            namedPipeServer.Dispose();
+            if (NamedPipeServer.IsConnected)
+                NamedPipeServer.Disconnect();
+            NamedPipeServer.Dispose();
             ServerThread = null;
         }
     }
