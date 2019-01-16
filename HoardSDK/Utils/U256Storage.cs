854b23d856aaf7f18a40de2ea2286ae518e96de6
rafalw <rw@hoard.exchange>
2019-01-16 14:35:13 +0100

    [SDK-279] U256 state packer/unpacker

---
+++ HoardSDK/Utils/U256Storage.cs
@@ -0,0 +1,210 @@
+﻿#if DEBUG
+#define ENABLE_U256STORAGE_VALIDATION
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+namespace Hoard.Utils
+{
+    /// <summary>
+    /// Item state storage
+    /// </summary>
+    public class U256Storage
+    {
+        private enum DataSize
+        {
+            UInt8           = 8,
+            UInt16          = 16,
+            UInt32          = 32,
+            UInt64          = 64,
+            Bool            = 8,
+
+            MaxStorageSize  = 256,
+        }
+
+        private BigInteger Bunch = new BigInteger();
+        private int ActualPackingShift;
+        private int ActualUnpackingShift;
+
+#if ENABLE_U256STORAGE_VALIDATION
+        private List<DataSize> DataList = new List<DataSize>();
+#endif
+
+        /// <summary>
+        /// Item state storage constructor
+        /// </summary>
+        public U256Storage()
+        {
+            Bunch = BigInteger.Zero;
+            ActualPackingShift = 0;
+            ActualUnpackingShift = 0;
+        }
+
+        /// <summary>
+        /// Item state storage creator
+        /// </summary>
+        /// <param state="value"> initial item state </param>
+        static public U256Storage Create(BigInteger state)
+        {
+            U256Storage storage = new U256Storage();
+            storage.Bunch = state;
+            return storage;
+        }
+
+        /// <summary>
+        /// Return available bits in storage
+        /// </summary>
+        /// <returns></returns>
+        public int GetBitsToPack()
+        {
+            return (int)DataSize.MaxStorageSize - ActualPackingShift;
+        }
+
+        /// <summary>
+        /// Pack value to storage
+        /// </summary>
+        /// <param state="value"> value </param>
+        public void PackUInt8(byte value)
+        {
+            Bunch |= new BigInteger(value) << ActualPackingShift;
+            ActualPackingShift += (int)DataSize.UInt8;
+            Debug.Assert(ActualPackingShift <= (int)DataSize.MaxStorageSize);
+#if ENABLE_U256STORAGE_VALIDATION
+            DataList.Add(DataSize.UInt8);
+#endif
+        }
+
+        /// <summary>
+        /// Pack value to storage
+        /// </summary>
+        /// <param state="value"> value </param>
+        public void PackUInt16(UInt16 value)
+        {
+            Bunch |= new BigInteger(value) << ActualPackingShift;
+            ActualPackingShift += (int)DataSize.UInt16;
+            Debug.Assert(ActualPackingShift <= (int)DataSize.MaxStorageSize);
+#if ENABLE_U256STORAGE_VALIDATION
+            DataList.Add(DataSize.UInt16);
+#endif
+        }
+
+        /// <summary>
+        /// Pack value to storage
+        /// </summary>
+        /// <param state="value"> value </param>
+        public void PackUInt32(UInt32 value)
+        {
+            Bunch |= new BigInteger(value) << ActualPackingShift;
+            ActualPackingShift += (int)DataSize.UInt32;
+            Debug.Assert(ActualPackingShift <= (int)DataSize.MaxStorageSize);
+#if ENABLE_U256STORAGE_VALIDATION
+            DataList.Add(DataSize.UInt32);
+#endif
+        }
+
+        /// <summary>
+        /// Pack value to storage
+        /// </summary>
+        /// <param state="value"> value </param>
+        public void PackUInt64(UInt64 value)
+        {
+            Bunch |= new BigInteger(value) << ActualPackingShift;
+            ActualPackingShift += (int)DataSize.UInt64;
+            Debug.Assert(ActualPackingShift <= (int)DataSize.MaxStorageSize);
+#if ENABLE_U256STORAGE_VALIDATION
+            DataList.Add(DataSize.UInt64);
+#endif
+        }
+
+        /// <summary>
+        /// Pack value to storage
+        /// </summary>
+        /// <param state="value"> value </param>
+        public void PackBool(bool value)
+        {
+            PackUInt8((byte)(value == true ? 0xff : 0x00));
+        }
+
+
+        /// <summary>
+        /// Unpack value from storage
+        /// </summary>
+        /// <returns></returns>
+        public byte UnpackUInt8()
+        {
+#if ENABLE_U256STORAGE_VALIDATION
+            Debug.Assert(DataList.Count > 0);
+            Debug.Assert(DataList[0] == DataSize.UInt8);
+            DataList.RemoveAt(0);
+#endif
+            byte value = (byte)((Bunch >> ActualUnpackingShift) & 0xff);
+            ActualUnpackingShift += (int)DataSize.UInt8;
+            Debug.Assert(ActualUnpackingShift <= (int)DataSize.MaxStorageSize);
+            return value;
+        }
+
+        /// <summary>
+        /// Unpack value from storage
+        /// </summary>
+        /// <returns></returns>
+        public UInt16 UnpackUInt16()
+        {
+#if ENABLE_U256STORAGE_VALIDATION
+            Debug.Assert(DataList.Count > 0);
+            Debug.Assert(DataList[0] == DataSize.UInt16);
+            DataList.RemoveAt(0);
+#endif
+            UInt16 value = (UInt16)((Bunch >> ActualUnpackingShift) & 0xffff);
+            ActualUnpackingShift += (int)DataSize.UInt16;
+            Debug.Assert(ActualUnpackingShift <= (int)DataSize.MaxStorageSize);
+            return value;
+        }
+
+        /// <summary>
+        /// Unpack value from storage
+        /// </summary>
+        /// <returns></returns>
+        public UInt32 UnpackUInt32()
+        {
+#if ENABLE_U256STORAGE_VALIDATION
+            Debug.Assert(DataList.Count > 0);
+            Debug.Assert(DataList[0] == DataSize.UInt32);
+            DataList.RemoveAt(0);
+#endif
+            UInt32 value = (UInt32)((Bunch >> ActualUnpackingShift) & 0xffffffff);
+            ActualUnpackingShift += (int)DataSize.UInt32;
+            Debug.Assert(ActualUnpackingShift <= (int)DataSize.MaxStorageSize);
+            return value;
+        }
+
+        /// <summary>
+        /// Unpack value from storage
+        /// </summary>
+        /// <returns></returns>
+        public UInt64 UnpackUInt64()
+        {
+#if ENABLE_U256STORAGE_VALIDATION
+            Debug.Assert(DataList.Count > 0);
+            Debug.Assert(DataList[0] == DataSize.UInt64);
+            DataList.RemoveAt(0);
+#endif
+            UInt64 value = (UInt64)((Bunch >> ActualUnpackingShift) & 0xffffffffffffffff);
+            ActualUnpackingShift += (int)DataSize.UInt64;
+            Debug.Assert(ActualUnpackingShift <= (int)DataSize.MaxStorageSize);
+            return value;
+        }
+
+        /// <summary>
+        /// Unpack value from storage
+        /// </summary>
+        /// <returns></returns>
+        public bool UnpackBool()
+        {
+            return UnpackUInt8() == 0xff ? true : false;
+        }
+    }
+}
---
+++ HoardTests/U256StorageTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HoardTests
+{
+    public class U256StorageTest
+    {
+        public U256StorageTest()
+        {
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task PackAndUnpackData()
+        {
+            const bool b1 = true;
+            const byte u8_1 = 0x13;
+            const byte u8_2 = 0xff;
+            const UInt64 u64_1 = 0xff00ff00ff00ff00;
+            const UInt16 u16_1 = 0xaabb;
+            const UInt32 u32_1 = 0xaaffaaff;
+            const bool b2 = true;
+            const bool b3 = false;
+            const bool b4 = true;
+            const UInt32 u32_2 = 0xaaffccff;
+
+            Hoard.Utils.U256Storage u256Storage = new Hoard.Utils.U256Storage();
+            u256Storage.PackBool(b1);
+            u256Storage.PackUInt8(u8_1);
+            u256Storage.PackUInt8(u8_2);
+            u256Storage.PackUInt64(u64_1);
+            u256Storage.PackUInt16(u16_1);
+            u256Storage.PackUInt32(u32_1);
+            u256Storage.PackBool(b2);
+            u256Storage.PackBool(b3);
+            u256Storage.PackBool(b4);
+            u256Storage.PackUInt32(u32_2);
+
+            Assert.Equal(u256Storage.UnpackBool(), b1);
+            Assert.Equal(u256Storage.UnpackUInt8(), u8_1);
+            Assert.Equal(u256Storage.UnpackUInt8(), u8_2);
+
+            Assert.Equal(u256Storage.UnpackUInt64(), u64_1);
+            Assert.Equal(u256Storage.UnpackUInt16(), u16_1);
+            Assert.Equal(u256Storage.UnpackUInt32(), u32_1);
+            Assert.Equal(u256Storage.UnpackBool(), b2);
+            Assert.Equal(u256Storage.UnpackBool(), b3);
+            Assert.Equal(u256Storage.UnpackBool(), b4);
+            Assert.Equal(u256Storage.UnpackUInt32(), u32_2);
+        }
+    }
+}
